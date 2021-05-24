0b83e7aa35cd8546c97b23b5d4169ff2533980c8
TugcenurGunduz <tnurgunduz@hotmail.com>
2021-05-24 20:50:26 +0300

    Add project files.

---
+++ Personel_Kayit/Frmistatistic.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Personel_Kayit
+{
+    public partial class Frmistatistic : Form
+    {
+        public Frmistatistic()
+        {
+            InitializeComponent();
+        }
+
+        SqlConnection baglanti = new SqlConnection("Data Source=DESKTOP-LQUHLSH\\SQLEXPRESS;Initial Catalog=PersonelVeriTabani;Integrated Security=True");
+
+        //Toplam Personel Sayısı
+        private void Frmistatistic_Load(object sender, EventArgs e)
+        {
+            baglanti.Open();
+            SqlCommand komut1 = new SqlCommand("Select Count(*) From Tbl_Personel", baglanti);
+            SqlDataReader dr1 = komut1.ExecuteReader();//veri okuyucu tanımladım.
+            while(dr1.Read())
+            {
+                LblToplamPersonel.Text = dr1[0].ToString();
+            }
+            baglanti.Close();
+        }
+    }
+}
